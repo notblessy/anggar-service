bb4072a9a3eae73ae9ab7fcd7c1ea808a6190dc6
Frederich Blessy <frederichblessy@gmail.com>
2025-02-18 17:18:55 +0700

    init app service

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"github.com/go-playground/validator"
+	"github.com/joho/godotenv"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/notblessy/anggar-service/db"
+	"github.com/notblessy/anggar-service/repository"
+	"github.com/notblessy/anggar-service/router"
+	"github.com/notblessy/anggar-service/utils"
+	"github.com/sirupsen/logrus"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		logrus.Warn("cannot load .env file")
+	}
+
+	postgres := db.NewPostgres()
+
+	e := echo.New()
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowHeaders: []string{
+			echo.HeaderOrigin,
+			echo.HeaderContentType,
+			echo.HeaderAccept,
+			echo.HeaderAuthorization,
+			"X-Path",
+		},
+	}))
+	e.Use(middleware.CORS())
+	e.Validator = &utils.Ghost{Validator: validator.New()}
+
+	userRepo := repository.NewUserRepository(postgres)
+
+	httpService := router.NewHTTPService()
+	httpService.RegisterPostgres(postgres)
+	httpService.RegisterUserRepository(userRepo)
+
+	httpService.Router(e)
+
+	e.Logger.Fatal(e.Start(":3400"))
+}
